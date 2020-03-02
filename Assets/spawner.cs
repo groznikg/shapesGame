740c34bb23c2bc6b0acb9b2e6834d986e0b28dda
groznikg <gasper.groznik@gmail.com>
2020-03-02 15:52:51 +0100

    Finished square

---
+++ Assets/spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class spawner : MonoBehaviour
+{
+
+    public float spawnRate = 1f;
+
+    public GameObject squarePrefab;
+
+    private float nextTimeToSpawn = 0f;
+    // Start is called before the first frame update
+
+
+    void Update()
+    {
+        if(Time.time >= nextTimeToSpawn) {
+            Instantiate(squarePrefab, Vector3.zero, Quaternion.identity);
+            nextTimeToSpawn = Time.time + 1f/spawnRate;
+        }
+    }
+}
